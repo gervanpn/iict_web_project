a9a8cbd85199645f600a4570733babae3bca0bd9
Gervan Nantel <gervann@yahoo.ca>
2021-01-05 21:12:41 -0500

    Added components for the website

--- iiCt/src/app/app.module.ts
+++ iiCt/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CompanyProfileComponent } from './company-profile/company-profile.component';
+import { OurServicesComponent } from './our-services/our-services.component';
+import { OurExpertiseComponent } from './our-expertise/our-expertise.component';
+import { ProjectWorkComponent } from './project-work/project-work.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    FooterComponent,
+    CompanyProfileComponent,
+    OurServicesComponent,
+    OurExpertiseComponent,
+    ProjectWorkComponent,
+    ContactUsComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
