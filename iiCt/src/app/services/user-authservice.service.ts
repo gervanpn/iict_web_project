79a18e072f0873139f8026ab5cec9960f031f6d6
lalaantika <sharmal0707@gmail.com>
2021-01-22 09:57:48 -0500

    login works

--- iiCt/src/app/services/user-authservice.service.ts
+++ iiCt/src/app/services/user-authservice.service.ts
@@ -3,14 +3,29 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserAuthserviceService {
+  // user: User;
 
   constructor(
     public afs: AngularFirestore, 
     public afAuth: AngularFireAuth, 
     public router:Router
-  ) { }
+  ) {
+  //   this.afAuth.authState.subscribe(user => {
+  //     if (user){
+  //       this.user = user;
+  //       localStorage.setItem('user', JSON.stringify(this.user));
+  //     } else {
+  //       localStorage.setItem('user', null);
+  //     }
+  //  })
+  }
+  async login(email: string, password: string) {
+    var result = await this.afAuth.signInWithEmailAndPassword(email, password)
+    this.router.navigate(['project-work']);
+  }
 }
