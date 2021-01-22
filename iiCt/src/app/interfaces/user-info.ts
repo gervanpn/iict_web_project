1d014def5164f8463f70f9f13e0c376e2619887a
Gervan Nantel <gervann@yahoo.ca>
2021-01-22 09:42:46 -0500

    added dates to User Info

--- iiCt/src/app/interfaces/user-info.ts
+++ iiCt/src/app/interfaces/user-info.ts
@@ -4,12 +4,16 @@ export class UserInfo {
     email: string;
     phone: string;
     city: string;
+    dateAdded: Date;
+    lastLogin: Date;
 
-    constructor(firstName:string, lastName:string, email:string, phone:string, city:string) {
+    constructor(firstName:string, lastName:string, email:string, phone:string, city:string, dateAdded: Date, lastLogin: Date) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phone = phone;
         this.city = city;
+        this.dateAdded = new Date();
+        this.lastLogin = new Date();
     }
 }
