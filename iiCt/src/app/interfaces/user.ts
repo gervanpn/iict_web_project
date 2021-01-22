774740981189819710cb8c238a2a86a00b14dfa7
lalaantika <sharmal0707@gmail.com>
2021-01-22 10:34:37 -0500

    signup works without uid

--- iiCt/src/app/interfaces/user.ts
+++ iiCt/src/app/interfaces/user.ts
@@ -1,6 +1,13 @@
-export interface User {
+export class User {
     uid: string;
     email: string;
     displayName: string;
     emailVerified: boolean;
+
+    constructor(uid:string, email:string, displayName:string, emailVerified: boolean) {
+        this.email = email;
+        this.uid = uid;
+        this.displayName = displayName
+        this.emailVerified = emailVerified
+    }
 }
--- iiCt/src/app/services/user-authservice.service.ts
+++ iiCt/src/app/services/user-authservice.service.ts
@@ -2,30 +2,42 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import { from } from 'rxjs';
+import { User } from '../interfaces/user';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserAuthserviceService {
-  // user: User;
+   userID: User | undefined;
 
   constructor(
     public afs: AngularFirestore, 
     public afAuth: AngularFireAuth, 
     public router:Router
   ) {
-  //   this.afAuth.authState.subscribe(user => {
-  //     if (user){
-  //       this.user = user;
-  //       localStorage.setItem('user', JSON.stringify(this.user));
-  //     } else {
-  //       localStorage.setItem('user', null);
-  //     }
-  //  })
+    this.afAuth.authState.subscribe(user => {
+        localStorage.setItem('user', JSON.stringify(this.userID));
+    })
   }
   async login(email: string, password: string) {
     var result = await this.afAuth.signInWithEmailAndPassword(email, password)
     this.router.navigate(['project-work']);
   }
+
+  async register(email: string, password: string, fname:string,
+    lname: string, phone:string, city:string) {
+    var result = await this.afAuth.createUserWithEmailAndPassword(email, password)
+    // let uid = (await this.afAuth.currentUser).uid
+    this.router.navigate(['user-page']);
+    return from( this.afs.collection('/UserInfo').doc().set({
+      firstName: fname, 
+      lastName: lname, 
+      email: email, 
+      phone: phone, 
+      city: city,
+    }))
+  }
+
 }
